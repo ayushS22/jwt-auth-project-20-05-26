022a83a0daa842e1e592e79710ac6bdb654dc874
ayushsahu <ayushsahu97810@gmail.com>
2026-05-20 17:45:44 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,34 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Connect Database
+
 connectDB();
 
-app.use(cors());
+// Middleware
 
 app.use(express.json());
 
+// CORS Configuration
+
+app.use(
+  cors({
+    origin: "https://jwt-auth-project-20-05-26-ew8x.vercel.app/",
+    credentials: true,
+  })
+);
+
+// Routes
+
 app.use("/api/auth", authRoutes);
+
+// Test Route
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
+// Server
 
 const PORT = process.env.PORT || 5000;
 
